a26a380b10a964882e1de3d15c52461f176fa55f
Eugenia <pyssiki@yandex.ru>
2022-07-29 15:23:49 +0300

    Initial Commit

---
+++ uuuu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uuuu
+//
+//  Created by Евгения Аникина on 29.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
